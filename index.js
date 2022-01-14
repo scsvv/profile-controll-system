14c60498a9ffbbb4126ae1e79167d9140b2b0b4a
Скоробогатов Святослав <45883075+CarbonSkorobogatov@users.noreply.github.com>
2022-01-14 19:05:30 +0200

    mockapp

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const PORT = 8030;
-const userRouter = require('./api/users')
+const mockapp = require('./api/mockapp')
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use('/users', userRouter);
+app.use('/mockapp', mockapp);
 
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`)
-  })
+})
